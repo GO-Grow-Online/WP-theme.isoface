400dfdc25e7d3db6d92b18635649f4205c7916b2
GO-Grow-Online <julien.growonline@gmail.com>
2023-10-10 10:11:22 +0200

    tweak

--- js/admin.js
+++ js/admin.js
@@ -1,20 +1,20 @@
-jQuery(function($) {
-    dynamic_title_repeater_accordion('rep', 't');
-    dynamic_title_repeater_accordion('collapses', 't');
-});
-
-function dynamic_title_repeater_accordion(repeater_name, field_name) {
-    var information_tabs = jQuery("div[data-name='" + repeater_name + "']");
-    if (information_tabs.length) {
-        var selector = "tr:not(.acf-clone) td.acf-fields .acf-accordion-content div[data-name='" + field_name + "'] input";
-
-        // Ajouter l'écouteur
-        jQuery(information_tabs).on('input', selector, function() {
-            var me = jQuery(this);
-            me.closest('td.acf-fields').find('.acf-accordion-title label').text(me.val());
-        });
-
-        // Déclencher la fonction lors du chargement
-        information_tabs.find(selector).trigger('input');
-    }
-}
+jQuery(function($) {
+    dynamic_title_repeater_accordion('rep', 't');
+    dynamic_title_repeater_accordion('collapses', 't');
+});
+
+function dynamic_title_repeater_accordion(repeater_name, field_name) {
+    var information_tabs = jQuery("div[data-name='" + repeater_name + "']");
+    if (information_tabs.length) {
+        var selector = "tr:not(.acf-clone) td.acf-fields .acf-accordion-content div[data-name='" + field_name + "'] input";
+
+        // Ajouter l'écouteur
+        jQuery(information_tabs).on('input', selector, function() {
+            var me = jQuery(this);
+            me.closest('td.acf-fields').find('.acf-accordion-title label').text(me.val());
+        });
+
+        // Déclencher la fonction lors du chargement
+        information_tabs.find(selector).trigger('input');
+    }
+}
